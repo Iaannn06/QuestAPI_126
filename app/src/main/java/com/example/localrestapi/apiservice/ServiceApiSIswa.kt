fe9de03a8e68501a396a796b8d7596cd2feb114c
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-18 16:09:33 +0700

    menambahkan ServiceApiSiswa

---
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSIswa.kt
@@ -0,0 +1,14 @@
+package com.example.localrestapi.apiservice
+
+import com.example.localrestapi.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+    @GET(value="bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST(value="InsertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+}
