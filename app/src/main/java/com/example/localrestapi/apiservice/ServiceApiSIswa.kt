4a10f318867be86206074307b3b8258a7d84913b
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-18 16:14:31 +0700

    menambahkan GET("baca1Teman.php/{id}, @PUT("editTM.php/{id}"), dan @DELETE("deleteTM.php/{id")

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSIswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSIswa.kt
@@ -11,4 +11,13 @@ interface ServiceApiSiswa {
 
     @POST(value="InsertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    // @GET("baca1Teman.php/{id}
+    //suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    //@PUT("editTM.php/{id}")
+    //suspend fun editSatuSiswa(@Query("id") id:Int,@Body, dataSiswa: DataSiswa):retrofit2.response<Void>
+
+    //@DELETE("deleteTM.php/{id")
+    //suspend fun hapusSatuSiswa(@Query("id") id:Int):retrofit2.Response<Void>
 }
