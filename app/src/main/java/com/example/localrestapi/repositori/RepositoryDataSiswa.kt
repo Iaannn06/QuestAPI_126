646d478d2c980ca1c00975e43d1fa0af81aae22f
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 14:24:14 +0700

    menambahkan suspend function getSatuSiswa

--- app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
@@ -6,7 +6,7 @@ import com.example.localrestapi.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa (): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    //suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
     //suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Responsive<Void>
     //suspend fun hapusSatuSiswa(id:Int): retrofit2.Responsive<Void>
 }
