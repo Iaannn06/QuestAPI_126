31bd234669700c9aad5c4fc4efe3a5633f36b988
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 14:17:21 +0700

    menambahkan function put untuk editSatuSiswa

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -4,6 +4,7 @@ import com.example.localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 interface ServiceApiSiswa {
@@ -16,8 +17,8 @@ interface ServiceApiSiswa {
     @GET("baca1Teman.php")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
-    //@PUT("editTM.php/{id}")
-    //suspend fun editSatuSiswa(@Query("id") id:Int,@Body, dataSiswa: DataSiswa):retrofit2.response<Void>
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id:Int,@Body, dataSiswa: DataSiswa):retrofit2.response<Void>
 
     //@DELETE("deleteTM.php/{id")
     //suspend fun hapusSatuSiswa(@Query("id") id:Int):retrofit2.Response<Void>
