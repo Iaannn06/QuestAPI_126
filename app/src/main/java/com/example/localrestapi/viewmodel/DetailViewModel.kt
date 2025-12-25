dccff53d9faa2da9d82179e9823bf7f81b4f6549
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 21:36:08 +0700

    menambahkan sealed interface untuk StatusUIDetail

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.localrestapi.viewmodel
 
+import com.example.localrestapi.modeldata.DataSiswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satuSiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
