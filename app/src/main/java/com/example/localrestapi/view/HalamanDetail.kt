819b4576dd75ebbcd9d9f7128565821c5aec2ceb
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-26 04:18:18 +0700

    menambahkan private function BodyDetailDataSiswa

--- app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
@@ -1,39 +1,67 @@
 package com.example.localrestapi.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.localrestapi.R
+import com.example.localrestapi.uicontroller.route.DestinasiEdit
+import com.example.localrestapi.viewmodel.EditViewModel
+import com.example.localrestapi.viewmodel.StatusUIDetail
+import com.example.localrestapi.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+private fun BodyDetailDataSiswa(
+    statusUIDetail: StatusUIDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
 
-fun DetailSiswaScreen(
-    navigateToEditItem: (Int) -> Unit,
-    navigateBack: () -> Unit,
-    modifier: Modifier = Modifier,
-    navigateUp = navigateBack
-){
-Scaffold(
-    topBar = {
-        SiswaTopAppBar(
-            title = stringResource(DestinasiDetail.titleRes),
-            canNavigateBack = true,
-            navigateUp = navigateBack
-        )
-    },
-    floatingActionButton = {
-        val uiState = viewModel.statusUiDetail
-        FloatingActionButton{
-            valUiState = viewModel.statusUIDetail
-            floatingActionButton(
-                o
+        when (statusUIDetail) {
+            is StatusUIDetail.Success -> DetailDataSiswa(
+                siswa = statusUIDetail.satusiswa,
+                modifier = Modifier.fillMaxWidth()
+            )
+            else -> {}
+        }
+
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
             )
         }
-        )
     }
-)
 }
