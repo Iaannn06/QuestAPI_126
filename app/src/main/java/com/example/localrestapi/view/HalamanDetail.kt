32bc9e81503b40a9cea969a111c4e26dcdae64c5
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 15:13:49 +0700

    menambahkan SiswaTopAppBar

--- app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
@@ -1,8 +1,10 @@
 package com.example.localrestapi.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api)
 @Composable
@@ -13,5 +15,13 @@ fun DetailSiswaScreen(
     modifier: Modifier = Modifier,
     navigateUp = navigateBack
 ){
-
+Scaffold(
+    topBar = {
+        SiswaTopAppBar(
+            title = stringResource(DestinasiDetail.titleRes),
+            canNavigateBack = true,
+            navigateUp = navigateBack
+        )
+    }
+)
 }
