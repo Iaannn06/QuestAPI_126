30d8be3041a80cf86ed137b520fe24779aa170f0
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 21:14:01 +0700

    menambahkan private val idSiswa

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -7,9 +7,21 @@ import com.example.localrestapi.modeldata.UIStateSiswa
 import com.example.localrestapi.repositori.RepositoryDataSiswa
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.toUiStateSiswa
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
   RepositoryDataSiswa): ViewModel(){
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
   private set
+
+  private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+  init {
+    viewModelScope.launch {
+      uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+        .toUiStateSiswa()
+    }
+  }
   }
