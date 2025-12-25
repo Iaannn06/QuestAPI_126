1f98b26e7c0af00607c1647b553c39c311e6d58a
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 21:11:26 +0700

    menambahkan class EditViewModel

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.localrestapi.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+  RepositoryDataSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+  private set
+  }
