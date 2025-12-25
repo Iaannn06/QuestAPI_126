0b546e89dda9be1744fda4971b2818334d2ae9d5
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 14:16:59 +0700

    menambahkan function getSatuSiswa

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -4,6 +4,7 @@ import com.example.localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET(value="bacaTeman.php")
@@ -12,12 +13,12 @@ interface ServiceApiSiswa {
     @POST(value="InsertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-    // @GET("baca1Teman.php/{id}
-    //suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
-    //
+    @GET("baca1Teman.php")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
     //@PUT("editTM.php/{id}")
     //suspend fun editSatuSiswa(@Query("id") id:Int,@Body, dataSiswa: DataSiswa):retrofit2.response<Void>
-    //
+
     //@DELETE("deleteTM.php/{id")
     //suspend fun hapusSatuSiswa(@Query("id") id:Int):retrofit2.Response<Void>
 }
