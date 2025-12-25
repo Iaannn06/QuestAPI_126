20b51b61b86c3aacb5e81e3e41aab6d07f45c0d1
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 19:58:12 +0700

    menambahkan object pada DestinasiDetail.kt

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,9 @@
 package com.example.localrestapi.uicontroller.route
 
+import com.example.localrestapi.R
+
+object DestinasiDetail: DestinasiNavigasi {
+ override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+}
