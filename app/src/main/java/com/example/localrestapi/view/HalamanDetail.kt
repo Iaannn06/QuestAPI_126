826a3cde12374f195d15381cf7fd2caee1b48a44
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 15:11:55 +0700

    menambahkan function DetailSiswaScreen

--- app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanDetail.kt
@@ -1,2 +1,17 @@
 package com.example.localrestapi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api)
+@Composable
+
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    navigateUp = navigateBack
+){
+
+}
