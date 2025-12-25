9765253e8f98a4ae0c3a7841114eb37068166c1e
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-26 03:51:21 +0700

    menambahkan function EditSiswaScreen

--- app/src/main/java/com/example/localrestapi/view/HalamanEdit.kt
+++ app/src/main/java/com/example/localrestapi/view/HalamanEdit.kt
@@ -1,2 +1,22 @@
 package com.example.localrestapi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.localrestapi.viewmodel.EditViewModel
+import com.example.localrestapi.viewmodel.provider.PenyediaViewModel
+import com.example.localrestapi.viewmodel.provider.PenyediaViewModel.Factory
+import kotlinx.coroutines.channels.Channel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+navigateBack: () -> Unit,
+onNavigateUp: () -> Unit,
+modifier: Modifier = Modifier,
+viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
