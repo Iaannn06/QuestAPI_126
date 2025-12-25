49509942ab816b6fa767bc5ff4bfd747f2d950e6
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 19:59:51 +0700

    menambahkan constraint dan value pada itemIdArg

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -3,7 +3,8 @@ package com.example.localrestapi.uicontroller.route
 import com.example.localrestapi.R
 
 object DestinasiDetail: DestinasiNavigasi {
- override val route = "detail_siswa"
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
-
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
