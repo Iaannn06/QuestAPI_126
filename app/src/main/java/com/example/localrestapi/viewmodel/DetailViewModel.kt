168b6c36b6263a70ffac34294b6964a1c500bd42
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-12-25 21:48:36 +0700

    menambahkan class DetailViewModel

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.localrestapi.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.localrestapi.modeldata.DataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satuSiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa): ViewModel() {
+
 }
 
